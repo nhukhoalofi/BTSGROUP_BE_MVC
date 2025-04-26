2f342a15247396a0bd55ff086fafcb9cb054d99d
nhukhoalofi <nhukhoacr7@gmail.com>
2025-04-26 09:41:16 +0700

    Add files via upload

--- src/apis/auth/auth_controller.js
+++ src/apis/auth/auth_controller.js
@@ -1,3 +1,4 @@
+//	Nhận request từ router, xử lý dữ liệu đầu vào và gọi auth_service để xử lý nghiệp vụ
 import AuthService from './auth_service.js';
 
 class AuthController {
@@ -5,6 +6,12 @@ class AuthController {
     try {
       const { name, email, password } = req.body;
       console.log('Request data:', req.body); 
+      if (!name || !email || !password) {
+        return res.status(400).json({
+          success: false,
+          message: 'Name, email and password are required'
+        });
+      }
 
       const newUser = await AuthService.register(name, email, password);
 
@@ -15,7 +22,85 @@ class AuthController {
       });
     } catch (err) {
       console.error('Error during registration:', err); 
-      return res.status(400).json({
+      const statusCode = err.statusCode || 400;
+      return res.status(statusCode).json({
+        success: false,
+        message: err.message || 'Đã xảy ra lỗi'
+      });
+    }
+  }
+  async login(req,res){
+    try {
+      const {email,password}=req.body;
+      if (!email || !password){
+        return res.status(400).json({
+          success: false,
+          message: 'Email, password are required'
+        })
+      }
+      const newUser= await AuthService.login(email,password);
+      return res.status(200).json({
+        success: true,
+        message: 'Login success',
+        data: newUser
+      })
+    }
+    catch(err){
+    console.error('Error during login:', err); 
+    const statusCode = err.statusCode || 400;
+    return res.status(statusCode).json({
+      success: false,
+      message: err.message || 'Đã xảy ra lỗi'
+    });
+    }
+  }
+  async forgotPassword(req, res) {
+    try {
+      const { email } = req.body;
+      
+      if (!email) {
+        return res.status(400).json({
+          success: false,
+          message: 'Email is required'
+        });
+      }
+      const resetToken = await AuthService.forgotPassword(email);
+  
+      return res.status(200).json({
+        success: true,
+        message: 'Password reset email sent'
+      });
+    } catch (err) {
+      console.error('Error during forgot password:', err);
+      const statusCode = err.statusCode || 400;
+      return res.status(statusCode).json({
+        success: false,
+        message: err.message || 'Đã xảy ra lỗi'
+      });
+    }
+  }
+
+  async resetPassword(req, res) {
+    try {
+      const { token, newPassword } = req.body;
+      
+      if (!token || !newPassword) {
+        return res.status(400).json({
+          success: false,
+          message: 'bạn đã nhập thiếu token hoặc newPassword'
+        });
+      }
+
+      await AuthService.resetPassword(token, newPassword);
+  
+      return res.status(200).json({
+        success: true,
+        message: 'Đặt lại mật khẩu thành công'
+      });
+    } catch (err) {
+      console.error('Error during reset password:', err);
+      const statusCode = err.statusCode || 400;
+      return res.status(statusCode).json({
         success: false,
         message: err.message || 'Đã xảy ra lỗi'
       });
