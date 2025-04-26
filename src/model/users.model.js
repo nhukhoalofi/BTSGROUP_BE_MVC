d928f15dac6683494d89f7dc7fdf212313a2208e
nhukhoalofi <nhukhoacr7@gmail.com>
2025-04-26 09:42:45 +0700

    Add files via upload

--- src/model/users.model.js
+++ src/model/users.model.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose';
+//Chứa các schema/model mô tả cấu trúc dữ liệu (vd: User, Product...), thường dùng với ORM như Mongoose, Sequelize...
+import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const userSchema= new mongoose.Schema({
+        name: {type:String, required: true},
+        email: {type:String, required:true},
+        password: {type:String, required:true},
+        resetPasswordToken: {type:String},
+        resetPasswordExpires: {type:String}     
 });
-
-const UserModel = mongoose.model('User', userSchema);
-
-export default UserModel;  // Đảm bảo xuất đúng cách như thế này
+const UserModel= mongoose.model('User',userSchema);//liên kết với colection users
+export default UserModel;
