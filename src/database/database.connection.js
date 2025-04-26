615863b9da81f1996950f30ce8f0480053b01525
nhukhoalofi <nhukhoacr7@gmail.com>
2025-04-26 09:42:06 +0700

    Add files via upload

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,14 +1,13 @@
-// src/database/database.connection.js
-import mongoose from 'mongoose';
-
-const connectDB = async () => {
+//Xử lý việc kết nối tới cơ sở dữ liệu (MongoDB, MySQL, v.v.), thường chỉ gọi một lần duy nhất khi server khởi động.
+import mongoose from "mongoose";
+const connectDB=async()=> {
   try {
     await mongoose.connect('mongodb://user:password@127.0.0.1:27019/S-Mongo?authSource=admin', { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
-  } catch (err) {
-    console.error('Failed to connect to MongoDB', err);
+  }
+  catch (err) {
+    console.error('Failed to connect to MongoDB',err);
     process.exit(1);
   }
 };
-
 export default connectDB;
